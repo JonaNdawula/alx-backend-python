4fdd1151a4dc51e4357006b7feabc2a26ca401e6
Jonathan Ndawula <jonathan.ndawula@yahoo.com>
2024-05-02 03:09:14 -0700

    code for 0-add.py

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+"""
+A module returning the sum of
+two given floats
+"""
+
+
+def add(a: float, b: float) -> float:
+    """
+    Returns the sum of two floats
+    """
+    return (a + b)
