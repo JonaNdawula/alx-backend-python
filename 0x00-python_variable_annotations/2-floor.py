a6fd0a9348fe4b788caea9ae782025b90e2f747b
Jonathan Ndawula <jonathan.ndawula@yahoo.com>
2024-05-02 03:28:47 -0700

    code for 2-floor.py

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+
+"""
+This module returns a whole number from a float input
+"""
+
+
+def floor(n: float) -> int:
+    """
+    This function returns a whole number from a float
+    """
+    return (int(n))
