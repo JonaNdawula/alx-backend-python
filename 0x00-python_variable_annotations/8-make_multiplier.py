a4f57bd393282a40f9829d813364e26cded239ff
Jonathan Ndawula <jonathan.ndawula@yahoo.com>
2024-05-02 04:30:08 -0700

    code for 8-make_multiplier.py

---
+++ 0x00-python_variable_annotations/8-make_multiplier.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env/ python3
+
+
+"""
+This module takes a float
+multiplier as an arg and returns a
+function that multiplies a float
+"""
+
+
+from typing import Callable
+
+
+def make_multiplier(multiplier: float) -> Callable[[float], float]:
+    """
+    This function returns another
+    function that multiplies floats
+    """
+    return (lambda k: k * multiplier)
