2fb837517a99cd17871a0f8801746add27123606
Jonathan Ndawula <jonathan.ndawula@yahoo.com>
2024-05-02 03:33:00 -0700

    code for 3-to_str.py

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+
+"""
+This module takes a float and returns a string
+"""
+
+
+def to_str(n: float) -> str:
+    """
+    This function returns a string
+    """
+    return (str(n))
