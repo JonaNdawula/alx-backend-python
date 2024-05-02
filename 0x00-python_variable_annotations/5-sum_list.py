dfe4fa02adb25004cb39ee6c00805b9880e3157b
Jonathan Ndawula <jonathan.ndawula@yahoo.com>
2024-05-02 03:53:55 -0700

    code for 5-sum_list.py

---
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+
+"""
+This is a module which takes a list as input
+and gives the list back as a sum
+"""
+
+from typing import List
+
+
+def sum_list(input_list: List[float]) -> float:
+    """
+    This function returns the sum of a list
+    """
+    return (sum(input_list))
