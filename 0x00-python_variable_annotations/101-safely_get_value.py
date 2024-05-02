c5d471ec5311754190675361b20466f0c0ea26bc
Jonathan Ndawula <jonathan.ndawula@yahoo.com>
2024-05-02 07:03:36 -0700

    code for 101-safely_get_value.py

---
+++ 0x00-python_variable_annotations/101-safely_get_value.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+
+
+"""
+This module uses type annotaions
+"""
+
+
+from typing import Mapping, Any, TypeVar, Union
+
+
+T = TypeVar('T')
+
+
+def safely_get_value(
+    dct: Mapping,
+    key: Any,
+    default: Union[T, None] = None
+) -> Union[Any, T]:
+    """
+    This function returns dct
+    """
+    if key in dct:
+        return dct[key]
+    else:
+        return default
