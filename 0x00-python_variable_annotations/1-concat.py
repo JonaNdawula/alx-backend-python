af2f5de2064c049b20f63540d10bb91843f2e1d7
Jonathan Ndawula <jonathan.ndawula@yahoo.com>
2024-05-02 03:23:03 -0700

    code for 1-concat.py

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+
+"""
+This module concatenates two strings
+"""
+
+
+def concat(first_string: str, second_string: str) -> str:
+    """
+    This function concaenates two strings to make a word
+    """
+    return (first_string + second_string)
