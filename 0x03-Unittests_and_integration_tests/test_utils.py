939d771bfaa78470ebbba27b1b9c0d0e11c922a2
Jonathan Ndawula <jonathan.ndawula@yahoo.com>
2024-05-25 10:35:07 -0700

    code for test_utils.py

---
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+"""
+This module contains
+unittets
+"""
+import unittest
+from parametized import parametized
+import utils
+
+
+class TestAccessNestedMap(unittest.TestCase):
+    """
+    TestAccessNestedMap class, tests
+    Map functions
+    """
+    @parametized.expand([
+        ({"a": 1}, ("a",), 1),
+        ({"a": {"b": 2}}, ("a",), {"b": 2}),
+        ({"a": {"b": 2}}, ("a", "b"), 2)
+    ])
+    def test_access_nested_map(self, nested_map, path, expected):
+        """
+        Tests if method returnscorrect
+        output
+        """
+        self.assertEqual(utils.access_nested_map(nested_map, path), expected)
