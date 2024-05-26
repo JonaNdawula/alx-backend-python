1df647001c15e25acbcc2b59105f08f6a733c704
Jonathan Ndawula <jonathan.ndawula@yahoo.com>
2024-05-26 07:20:51 -0700

    code for 5. Mocking a property

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -21,7 +21,6 @@ class TestGithubOrgClient(unittest.TestCase):
         ("google",),
         ("abc",)
     ])
-
     @patch('client.get_json', return_value={"payload": True})
     def test_org(self, org_name, mock_get):
         """
@@ -33,3 +32,15 @@ class TestGithubOrgClient(unittest.TestCase):
         self.assertEqual(response, {"payload": True})
         expected = f"https://api.github.com/orgs/{org_name}"
         mock_get.assert_called_once_with(expected)
+
+    @path('client.GithubOrgClient.org', new_callable=PropertyMock)
+    def test_public_repos_url(self, mock_org):
+        """
+        This tests GithubOrgClients._public_repos_url
+        """
+        payload = {"repos_url": "https://api.github.com/orgs/octocat/repos"}
+        mock_org.return_value = payload
+        test_client = GithubOrgClient("octocat")
+        self.assertEqual(
+            test_client._public_repos_url,
+            payload["repos_url"])
